2d1837517da739e3d327a41dd08fef92f3b21619
Сергей Васильченко <svasilchenko@MacBook-Air-Sergej.local>
2021-11-11 21:28:07 +0300

    remove prin and add gitignore

--- tests/test_get_local_number.py
+++ tests/test_get_local_number.py
@@ -3,5 +3,4 @@ from src.MainClass import MainClass
 
 def test_get_local_number():
     local_number = MainClass()
-    print(local_number.get_local_number())
     assert local_number.get_local_number() == 14, "Число не равняется 14"
