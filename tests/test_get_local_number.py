b8bfd76badc2dab8c7eab5b3eca1d69072c56911
Сергей Васильченко <svasilchenko@MacBook-Air-Sergej.local>
2021-11-11 21:58:41 +0300

    add test for get class number

--- tests/test_get_local_number.py
+++ tests/test_get_local_number.py
@@ -2,5 +2,5 @@ from src.MainClass import MainClass
 
 
 def test_get_сlass_number():
-    private_number = MainClass()
-    assert private_number.get_local_number() > 45, "Число больше 45"
+    local_number = MainClass()
+    assert local_number.get_local_number() == 20, "Число не равняется 20"
