419d3d533e7799ea52bbccb1e7ac9d8301da190b
Сергей Васильченко <svasilchenko@MacBook-Air-Sergej.local>
2021-12-26 17:38:18 +0300

    add changes for platform launch in conftest

--- conftest.py
+++ conftest.py
@@ -1,22 +1,37 @@
+import os
+
 import pytest
 from appium import webdriver
 
 
-@pytest.fixture(scope='session')
-def driver(request):
-    desired_capabilities = {
-        "platformName": "Android",
-        "platformVersion": "11",
-        "automationName": "UIAutomator2",
-        "deviceName": "TestAndroid",
-        "app": "/Users/svasilchenko/PycharmProjects/PythonAppiumAutomation/apks/org.wikipedia.apk",
-        "packageName": "org.wikipedia/org.wikipedia.main.MainActivity",
-        "appPackage": "org.wikipedia",
-        "appActivity": ".main.MainActivity"
-    }
+def get_capabilities_by_platform_env():
+    platform = os.environ.get('PLATFORM')
+    PLATFORM_IOS = 'ios'
+    PLATFORM_ANDROID = 'android'
 
-    driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", desired_capabilities)
+    if platform == PLATFORM_ANDROID:
+        desired_capabilities = {
+            "platformName": "Android",
+            "platformVersion": "11",
+            "automationName": "UIAutomator2",
+            "deviceName": "TestAndroid",
+            "app": "/Users/svasilchenko/PycharmProjects/PythonAppiumAutomation/apks/org.wikipedia.apk",
+            "packageName": "org.wikipedia/org.wikipedia.main.MainActivity",
+            "appPackage": "org.wikipedia",
+            "appActivity": ".main.MainActivity"
+        }
+    elif platform == PLATFORM_IOS:
+        desired_capabilities = {"platformName": "iOS",
+                                "deviceName": "iPhone 13",
+                                "platformVersion": "15.2",
+                                "app": "/Users/svasilchenko/Desktop/test_appium_java/apks/Wikipedia.app"}
+    else:
+        raise Exception("Cannot get run platform from env variable. Platform value " + str(platform))
+    return desired_capabilities
+
+
+@pytest.fixture(scope='function')
+def driver(request):
+    driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", desired_capabilities=get_capabilities_by_platform_env())
     request.addfinalizer(driver.quit)
     return driver
-
-
